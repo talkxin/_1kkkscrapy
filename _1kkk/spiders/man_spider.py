f3ea8e572fdd3a26919501dec258747df143c191
liuxin <liuxin01@sdc.icbc.com.cn>
2017-02-01 22:01:29 +0800

    1

--- _1kkk/spiders/man_spider.py
+++ _1kkk/spiders/man_spider.py
@@ -33,7 +33,7 @@ class ManSpider(scrapy.Spider):
     cap["phantomjs.page.settings.loadImages"] = False
     cap["phantomjs.page.settings.disk-cache"] = True
     driver = webdriver.PhantomJS(executable_path=phantomjspath,desired_capabilities=cap)
-    driver.set_page_load_timeout(30)
+#    driver.set_page_load_timeout(30)
     """
         获取数据库中所有需要爬取的漫画
     """
@@ -187,7 +187,7 @@ class ManSpider(scrapy.Spider):
                 self.driver.quit()
                 self.driver = None
                 self.driver = webdriver.PhantomJS(executable_path=phantomjspath,desired_capabilities=self.cap)
-                self.driver.set_page_load_timeout(30)
+#                self.driver.set_page_load_timeout(30)
                 self.driver.get(furl)
                 time.sleep(3)
                 return self.getImgUrl(furl,jsurl,max,path)
