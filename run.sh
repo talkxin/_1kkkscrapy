e681a0001c17bb63dafc12ac8afb582728b2a9ce
liuxin <liuxin01@sdc.icbc.com.cn>
2017-02-07 08:06:30 +0800

    修改启动脚本

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh                                                                                                                                            
 
+export PATH=$PATH:/usr/local/bin
+
 cd /root/_1kkkscrapy/
 scrapy=`ps -C scrapy | awk 'NR==2{print $1}'`
 if [ ! $scrapy ]; then 
-nohup scrapy crawl manhua > example.log 2>&1 &
+nohup scrapy crawl manhua >> example.log 2>&1 &
 fi
